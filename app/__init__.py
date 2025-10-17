154e0c6d600b3af0caffde86e9b649e798ecdd8e
Stephanie Lin <slin.animation@gmail.com>
2025-10-17 12:57:25 -0400

    adds get_one_book route

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask
-# from .routes.hello_world_routes import hello_world_bp
+from .routes.book_routes import books_bp
 
 def create_app():
     app = Flask(__name__)
 
     # Register Blueprints here
+    app.register_blueprint(books_bp)
 
     return app
---
+++ app/routes/book_routes.py
@@ -0,0 +1,28 @@
+from flask import Blueprint
+from app.models.book import books
+
+books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
+
+@books_bp.get("")
+def get_all_books():
+    books_response = []
+    for book in books:
+        books_response.append(
+            {
+                "id" : book.id,
+                "title" : book.title,
+                "description" : book.description
+            }
+        )
+    return books_response
+
+@books_bp.get("/<book_id>")
+def get_book(book_id):
+    book_id = int(book_id)
+    for book in books:
+        if book.id == book_id:
+            return {
+                "id" : book.id,
+                "title" : book.title,
+                "description" : book.description,
+            }
